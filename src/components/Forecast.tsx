c6f0ed27b689c62ecc48999e96641050bbb51fb8
artёm rudoy <art.rudy@gmail.com>
2024-03-19 21:31:41 +0200

    small changes

--- src/components/Forecast.tsx
+++ src/components/Forecast.tsx
@@ -4,14 +4,30 @@ type Props = {
   data: forecastType
 }
 
+const Degree = ({ temp }: { temp: number }): JSX.Element => (
+  <span>
+    {temp}
+    <sup>o</sup>
+  </span>
+)
+
 const Forecast = ({ data }: Props): JSX.Element => {
+  const today = data.list[0]
   return (
     <div className="w-full md:max-w-[500px] py-4 md: py-4 md:px-10 lg:px-24 h-full lg:h-auto bg-white bg-opacity-20 background-blur-1s rounded drop-shadow-lg ">
       <div className="mx-auto w-[300px]">
         <section>
           <h2 className="text-2xl font-black">
-            {data.name}, <span> {data.country}</span>
+            {data.name}, <span className="font-thin"> {data.country}</span>
           </h2>
+          <h1 className="text-4xl font-extrabold">
+            <Degree temp={Math.round(today.main.temp)} />
+          </h1>
+          <p className="text-sm">
+            {' '}
+            {today.weather[0].main}
+            {today.weather[0].description}
+          </p>
         </section>
       </div>
     </div>
