550411341ccef73eada1b76cde212da49190e748
artёm rudoy <art.rudy@gmail.com>
2024-03-21 21:23:31 +0200

    Tile component created

--- src/components/Forecast.tsx
+++ src/components/Forecast.tsx
@@ -1,4 +1,7 @@
+import { getSunTime } from '../helpers'
 import { forecastType } from '../types'
+import Sunrise from './Icons/Sunrise'
+import Sunset from './Icons/Sunset'
 
 type Props = {
   data: forecastType
@@ -53,6 +56,18 @@ const Forecast = ({ data }: Props): JSX.Element => {
               </p>
             </div>
           ))}
+        </section>
+
+        <section className="flex justify-between text-zinc-700">
+          <div className="w-[140px] text-xs font-bold flex flex-col items-center bg-white/20 background-blur-ls rouned drop-shadow-lg py-4 mb-5">
+            <Sunrise />
+            <span className="mt-2">{getSunTime(data.sunrise)}</span>
+          </div>
+
+          <div className="w-[140px] text-xs font-bold flex flex-col items-center bg-white/20 background-blur-ls rouned drop-shadow-lg py-4 mb-5">
+            <Sunset />
+            <span className="mt-2"> {getSunTime(data.sunset)}</span>
+          </div>
         </section>
       </div>
     </div>
---
+++ src/components/Tile.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Tile = () => {
+  return (
+    <div>
+      <p> Tile</p>
+    </div>
+  )
+}
+export default Tile
